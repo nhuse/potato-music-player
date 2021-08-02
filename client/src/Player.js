3459499f1fe39e2b50c406dd1c1045429868fec2
TaraMKelly <tara.kelly16@gmail.com>
2021-08-02 11:13:13 -0400

    i did too much. using the built in spotify player

--- client/src/Player.js
+++ client/src/Player.js
@@ -1,28 +1,10 @@
-import React, { useState, useEffect, useRef }   from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import SpotifyPlayer from 'react-spotify-web-playback'
-
-// There are three state values that we should maintain:
-
-// trackIndex - The index of the track that's being played.
-// trackProgress - The current progress of the track.
-// isPlaying - Whether or not the track is being played.
-
-function Player({ tracks }) {
-
-    const [trackIndex, setTrackIndex] = useState(0);
-    const [trackProgress, setTrackProgress] = useState(0);
-    const [isPlaying, setIsPlaying] = useState(false);
-
-    return (
-
-    )
-
-}
 
 
 export default function Player({ accessToken }) {
     return <SpotifyPlayer token={accessToken}
-    showSaveIcon
-    uris={[]}
+        showSaveIcon
+        uris={[]}
     />
 }
