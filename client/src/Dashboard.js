f4c97816ba5516626d6f269ef6afe6d313586ede
TaraMKelly <tara.kelly16@gmail.com>
2021-08-02 10:16:39 -0400

    slowly adding things to establish state

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -15,7 +15,7 @@ export default function Dashboard({ code }) {
                 <PlaylistBar accessToken={accessToken}/>
             </div>
             <div>
-                {/* <Player accessToken={accessToken} /> */}
+                <Player accessToken={accessToken} />
             </div>
         </div>
     )
--- client/src/Player.js
+++ client/src/Player.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect, useRef }   from 'react'
 import SpotifyPlayer from 'react-spotify-web-playback'
 
+// There are three state values that we should maintain:
 
-function Player {
+// trackIndex - The index of the track that's being played.
+// trackProgress - The current progress of the track.
+// isPlaying - Whether or not the track is being played.
+
+function Player({ tracks }) {
+
+    const [trackIndex, setTrackIndex] = useState(0);
+    const [trackProgress, setTrackProgress] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     return (
-        
+
     )
 
 }
