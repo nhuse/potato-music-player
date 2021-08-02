27dcdadd0c1ee7a6d0f8af87a7cd17193acfd102
TaraMKelly <tara.kelly16@gmail.com>
2021-08-02 09:50:24 -0400

    start building audio player

--- client/src/Player.js
+++ client/src/Player.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import SpotifyPlayer from 'react-spotify-web-playback'
 
+
+function Player {
+
+    return (
+        
+    )
+
+}
+
+
 export default function Player({ accessToken }) {
     return <SpotifyPlayer token={accessToken}
     showSaveIcon
