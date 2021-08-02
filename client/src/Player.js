1974dfa76900e7816a7bbdde975192e804a3d242
TaraMKelly <tara.kelly16@gmail.com>
2021-08-02 12:00:35 -0400

    added some props to the player, will switch to my branch

--- client/src/Player.js
+++ client/src/Player.js
@@ -4,5 +4,18 @@ import SpotifyPlayer from 'react-spotify-web-playback'
 export default function Player({ accessToken }) {
     return <SpotifyPlayer token={accessToken}
     uris={[]}
+    styles={{
+        activeColor: '#fff',
+        bgColor: '#333',
+        color: '#fff',
+        loaderColor: '#fff',
+        sliderColor: '#1cb954',
+        trackArtistColor: '#ccc',
+        trackNameColor: '#fff',
+      }}
+      initialVolume={1}
+      play={false}
+      showSaveIcon={true}
+
     />
 }
