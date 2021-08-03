cd54c3fe52b215d956a05518f943738cc29707de
TaraMKelly <tara.kelly16@gmail.com>
2021-08-03 09:16:07 -0400

    added player functionality

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import useAuth from './useAuth'
+// import SpotifyPlayer from 'react-spotify-web-playback'
 import Player from './Player'
 import PlaylistBar from './PlaylistBar'
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
+
+    const [playingTrack, setPlayingTrack] = useState()
+    
+    //needs to be passed down to use in a click event handler so that the clicked track plays
+    //the track data will pass from the playlist/search components
+    function chooseTrack(track) {
+        setPlayingTrack(track)
+    }
 
     return (
         <div>
@@ -15,7 +24,8 @@ export default function Dashboard({ code }) {
                 <PlaylistBar accessToken={accessToken}/>
             </div>
             <div>
-                <Player accessToken={accessToken} />
+                {/* will need track data from search/playlist components */}
+                <Player accessToken={accessToken} trackUri={playingTrack?.uri}/>
             </div>
         </div>
     )
--- client/src/Player.js
+++ client/src/Player.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import SpotifyPlayer from 'react-spotify-web-playback'
 
 
-export default function Player({ accessToken }) {
+export default function Player({ accessToken, trackUri }) {
+
+    const [play, setPlay] = useState(false)
+
+    useEffect(() => setPlay(true), [trackUri])
+    
+    if (!accessToken) return null
     return <SpotifyPlayer token={accessToken}
         showSaveIcon
-        uris={[]}
+        callback={state => {
+            if(!state.isPlaying) setPlay(false)
+        }}
+        play={play}
+        initialVolume
+        uris={trackUri ? [trackUri] : []}
+        styles={{
+            activeColor: '#fff',
+            bgColor: '#333',
+            color: '#fff',
+            loaderColor: '#fff',
+            sliderColor: '#1cb954',
+            trackArtistColor: '#ccc',
+            trackNameColor: '#fff',
+        }}
     />
 }
